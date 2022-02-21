3cea9a00a88c347c1e345af07d24a7544a8c44f8
Csprier <csprier@gmail.com>
2022-02-21 06:39:27 -0500

    Edited and commented server.js for clarity.

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
-// const { Client } = require('pg');
+// =============================================================
+// Create the Express app instance
 const express = require('express');
 const app = express();
+
+// =============================================================
+// dotenv
+const dotenv = require('dotenv');
+dotenv.config();
+
+// =============================================================
+app.use(express.json());
+app.use(cors());
+app.options('*', cors());
 const PORT = 8080;
 
-// Serve the public folder
-app.use(express.static('public'));
-
+// =============================================================
 // Routing
 const greetingRouter = require('./routes/greeting.routes');
 app.use(greetingRouter);
 
+// =============================================================
+// Serve the public folder
+app.use(express.static('public'));
 app.get('/', async (req, res) => {
   res.setHeader('Content-Type', "text/html");
   res.status(200);
   res.send('<h1>Hello world!</h1>');
 });
 
+// =============================================================
+// Listen to the PORT
 (async () => {
   const db = require('./util/db');
   await db.connect();
